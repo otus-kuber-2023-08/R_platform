f4d414913a07af9d172b74a83e12b61b52e13e2a
Robert Mazitov <maz.robert@mail.ru>
2024-01-24 02:01:21 +0300

    Configured services

--- kubernetes-project/configure_cluster.sh
+++ kubernetes-project/configure_cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Example https://github.com/stevesea/argocd-helm-app-of-apps-example/blob/master/README.md
+
 # Get latest kube config
 cat kubespray/inventory/sample/artifacts/admin.conf > ~/.kube/config
 
@@ -10,3 +12,17 @@ kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisione
 helm upgrade --install -n argo --create-namespace argo bitnami/argo-cd --set global.storageClass=local-path
 # Configure argo
 kubectl apply -f manifests
+
+#export vault_conf=$(kubectl -n infra exec -it vault-server-0 -- vault operator init --key-shares=1 --key-threshold=1)
+
+#echo $vault_conf
+
+#unseal_key=$(echo $vault_conf | grep Unseal | sed 's/Unseal Key 1: //g')
+#root_key=$(echo $vault_conf | grep Root | sed 's/Root Token //g')
+
+# todo make this for each vault
+#kubectl -n infra exec -it vault-server-0 -- vault operator unseal $unseal_key
+
+# todo set initial root Toeken here
+#kubectl -n infra exec -it vault-server-0 -- vault login
+#$(root_key)
